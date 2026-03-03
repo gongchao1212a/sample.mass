0a02c91102342c02b0c60cbcc02ff7d0a154e40f
gongc <gongc@wooduan.com>
2026-03-03 18:25:14 +0800

    测试代码

---
+++ Source/MassSample/Mass/AI/StateTreeTask_CustomMove.cpp
@@ -0,0 +1,37 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "StateTreeTask_CustomMove.h"
+
+
+
+#include "MassMovementFragments.h"
+#include "MassStateTreeExecutionContext.h"
+#include "StateTreeLinker.h"
+
+bool FStateTreeTask_CustomMove::Link(FStateTreeLinker& Linker)
+{
+    Linker.LinkExternalData(Location);
+    Linker.LinkExternalData(VelocityHandle);
+    Linker.LinkExternalData(MassSignalSubsystemHandle);
+    return true;
+}
+
+EStateTreeRunStatus FStateTreeTask_CustomMove::EnterState(FStateTreeExecutionContext& Context,
+    const FStateTreeTransitionResult& Transition) const
+{
+
+    const FMassStateTreeExecutionContext& MassContext = static_cast<FMassStateTreeExecutionContext&>(Context);
+    FMassVelocityFragment& MassVelocityFragment = MassContext.GetExternalData(VelocityHandle);
+    MassVelocityFragment.Value = { FMath::FRandRange(-100.f, 100.f), FMath::FRandRange(-100.f, 100.f), 0 };
+
+    UMassSignalSubsystem& MassSignalSubsystem = MassContext.GetExternalData(MassSignalSubsystemHandle);
+    MassSignalSubsystem.DelaySignalEntity(UE::Mass::Signals::StandTaskFinished, MassContext.GetEntity(), 2);
+
+    return EStateTreeRunStatus::Running;
+}
+
+EStateTreeRunStatus FStateTreeTask_CustomMove::Tick(FStateTreeExecutionContext& Context, const float DeltaTime) const
+{
+    return EStateTreeRunStatus::Succeeded;
+}
